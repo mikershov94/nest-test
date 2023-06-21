5b59b9d880edcde64e148e65c7d88bbdde24b289
Michael E. Ershov <em.ershov@yandex.ru>
2023-06-21 10:40:34 +0800

    Added cat service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get(':id')
+  findOne(@Param() params, @Res({ passthrough: true }) res: Response) {
+    res.status(HttpStatus.OK);
+    return { id: params.id, cat: params.id };
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findAll(@Res({ passthrough: true }) res: Response) {
+    res.status(HttpStatus.OK);
+    return [];
   }
 }
